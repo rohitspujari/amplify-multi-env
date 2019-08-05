4313ceb5ba3b3df09d732766a13e951a48f20890
Rohit Pujari <rohitspujari@gmail.com>
2019-08-05 10:09:58 -0500

    another commit

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Dev Branch
+          Learn React - Dev Branch of Amplify
         </a>
       </header>
     </div>
