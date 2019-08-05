2b30713be4dcd74ad5eb17fe9e85b92bf2bc2510
Rohit Pujari <rohitspujari@gmail.com>
2019-08-05 10:08:41 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - Dev Branch
         </a>
       </header>
     </div>
